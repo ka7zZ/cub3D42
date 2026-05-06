6b9e6699f32853921a08781dd48f890dc8da5446
Victor Ruiz Ruiz <vruiz-ru@c2r1s5.42madrid.com>
2026-05-06 16:56:32 +0200

    norm

--- srcs/mandatory/game/ft_game_loop.c
+++ srcs/mandatory/game/ft_game_loop.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_game_loop.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aghergut <aghergut@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: vruiz-ru <vruiz-ru@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 17:28:35 by vruiz-ru          #+#    #+#             */
-/*   Updated: 2026/05/06 15:05:12 by aghergut         ###   ########.fr       */
+/*   Updated: 2026/05/06 16:48:01 by vruiz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,36 +40,29 @@ static int	ft_update_delta_time(t_game *game)
 
 int	ft_check_wall(t_game *game, double x, double y)
 {
-	int		ix;
-	int		iy;
-	int		m[2];
-	int		map_w;
-	int		map_h;
-	char	**map;
+	int		i[2];
+	int		mx;
+	int		my;
 
-	map_w = game->map.map_width;
-	map_h = game->map.map_height;
-	map = game->map.map;
-	iy = -1;
-	while (iy <= 1)
+	i[0] = -1;
+	while (i[0] <= 1)
 	{
-		ix = -1;
-		while (ix <= 1)
+		i[1] = -1;
+		while (i[1] <= 1)
 		{
-			if (ix == 0 && iy == 0)
+			if (i[0] != 0 || i[1] != 0)
 			{
-				ix++;
-				continue ;
+				mx = (int)(x + i[1] * PLAYER_RADIUS);
+				my = (int)(y + i[0] * PLAYER_RADIUS);
+				if (mx < 0 || mx >= game->map.map_width
+					|| my < 0 || my >= game->map.map_height)
+					return (1);
+				if (game->map.map[my][mx] == '1')
+					return (1);
 			}
-			m[0] = (int)(x + ix * PLAYER_RADIUS);
-			m[1] = (int)(y + iy * PLAYER_RADIUS);
-			if (m[0] < 0 || m[0] >= map_w || m[1] < 0 || m[1] >= map_h)
-				return (1);
-			if (map[m[1]][m[0]] == '1')
-				return (1);
-			ix++;
+			i[1]++;
 		}
-		iy++;
+		i[0]++;
 	}
 	return (0);
 }
@@ -81,21 +74,18 @@ int	ft_game_loop(t_game *game)
 	long long	player_update_time;
 	long long	raycast_time;
 	long long	total_time;
-	
-	if (game->is_pressed == false) 
+
+	if (game->is_pressed == false)
 	{
 		frame_start = ft_now_us();
-	
 		ft_update_delta_time(game);
 		delta_calc_time = ft_now_us() - frame_start;
-		
 		game->cron.frame_count++;
 		ft_update_player(game);
 		player_update_time = ft_now_us() - frame_start - delta_calc_time;
-		
 		ft_raycast_frame(game);
-		raycast_time = ft_now_us() - frame_start - delta_calc_time - player_update_time;
-		
+		raycast_time = ft_now_us() - frame_start - delta_calc_time
+			- player_update_time;
 		total_time = ft_now_us() - frame_start;
 	}
 	return (0);
--- srcs/mandatory/game/ft_key_hooks_utils.c
+++ srcs/mandatory/game/ft_key_hooks_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_key_hooks_utils.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aghergut <aghergut@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: vruiz-ru <vruiz-ru@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 14:15:47 by aghergut          #+#    #+#             */
-/*   Updated: 2026/05/06 14:23:49 by aghergut         ###   ########.fr       */
+/*   Updated: 2026/05/06 16:53:18 by vruiz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,47 +57,5 @@ int	ft_key_press(int keycode, t_game *game)
 int	ft_key_release(int keycode, t_game *game)
 {
 	ft_clear_move_key(keycode, game);
-	return (0);
-}
-
-int	ft_mouse_pressed(int button, int x, int y, t_game *game)
-{
-	(void)button;
-	(void)x;
-	(void)y;
-	game->is_pressed = true;
-	game->input.key_w = 0;
-	game->input.key_s = 0;
-	game->input.key_a = 0;
-	game->input.key_d = 0;
-	game->input.key_left = 0;
-	game->input.key_right = 0;
-	return (1);
-}
-
-int	ft_mouse_released(int button, int x, int y, t_game *game)
-{
-	(void)button;
-	(void)x;
-	(void)y;
-	game->is_pressed = false;
-	return (1);
-}
-
-int	ft_focus_out(t_game *game)
-{
-	game->is_pressed = true;
-	game->input.key_w = 0;
-	game->input.key_s = 0;
-	game->input.key_a = 0;
-	game->input.key_d = 0;
-	game->input.key_left = 0;
-	game->input.key_right = 0;
-	return (0);
-}
-
-int	ft_focus_in(t_game *game)
-{
-	game->is_pressed = false;
 	return (0);
 }
